d4a03a6f716e0b37601f35b5e4f2facb21270e50
yancaolu <lujun964@163.com>
2019-11-08 23:29:59 +0800

    if判断

--- demo1/src/com/bianyiit/Demo1.java
+++ demo1/src/com/bianyiit/Demo1.java
@@ -4,5 +4,8 @@ public class Demo1 {
     public static void main(String[] args) {
 
         System.out.println("github");
+        if(1==1){
+            System.out.println("github");
+        }
     }
 }
